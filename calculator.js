cdfd87e8ee2d46e42e1e6befd2514590aa2149b3
Kevin Mooney <kevin.mooney@hcl.com>
2022-12-08 14:47:55 -0500

    Initial calculator layout and css

---
+++ calculator.js
@@ -0,0 +1,35 @@
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(operator, x, y) {
+    switch (operator) {
+        case 'add':
+            return add(x, y);
+            break;
+        case 'subtract':
+            return subtract(x, y);
+            break;
+        case 'multiply':
+            return multiply(x, y);
+            break;
+        case 'divide':
+            return divide(x, y);
+            break;
+        default:
+            console.log("Error '" + operator + "' is not a valid operator");
+    }
+}
